f171f11b25f25319ee346cbbfe805fd7c732571d
尬k <2950483571@qq.com>
2022-07-22 09:54:32 +0800

    第二次提交

--- src/global/register-element.js
+++ src/global/register-element.js
@@ -5,10 +5,16 @@ import {
   Header,
   Aside,
   Main,
-  Icon
+  Icon,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Row,
+  Col,
 } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-const components = [Button, Table, Container,Header,Aside,Main,Icon]
+const components = [Button, Table, Container, Header, Aside, Main, Icon, Menu,Submenu,MenuItem,MenuItemGroup,Row,Col,]
 export default function (Vue) {
   for (const component of components) {
     Vue.use(component)
