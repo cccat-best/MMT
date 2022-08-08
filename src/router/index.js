31456da6916aea3c648014a9b080bc861b75e046
yummyX02 <1245191297@qq.com>
2022-08-08 11:11:59 +0800

    测试登录部分

--- src/router/index.js
+++ src/router/index.js
@@ -104,7 +104,7 @@ router.beforeEach((to, from, next) => {
     return next()
   }
   let cookies = document.cookie
-  console.log(cookies);
+  console.log(cookies)
   if (!cookies) {
     return next('/login')
   }
