15dd6d41bb3168e1e9aa97af1cc6f6cb63cbe68c
jiaxing gao <79193821+gaoxiaoxin@users.noreply.github.com>
2022-08-29 10:02:23 +0800

    feat/预留axios请求

--- src/service/index.js
+++ src/service/index.js
@@ -1,10 +1,13 @@
-import axios from 'axios'
+// axios 请求封装预留
+// 前期没有考虑到进行请求的封装
 
-axios.interceptors.request.use((req) => {
-  console.log('我被调用啦')
-  console.log(req)
-})
+// import axios from 'axios'
 
-axios.interceptors.response.use((res) => {
-  console.log(res)
-})
+// axios.interceptors.request.use((req) => {
+//   console.log('我被调用啦')
+//   console.log(req)
+// })
+
+// axios.interceptors.response.use((res) => {
+//   console.log(res)
+// })
